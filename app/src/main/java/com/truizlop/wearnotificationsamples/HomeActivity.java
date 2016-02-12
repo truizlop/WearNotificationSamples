51a32da295d463f27089f13cdd7f4149064050fc
Tomas Ruiz-Lopez <trl.ironside@gmail.com>
2016-02-12 22:00:49 +0100

    Add plain notification support

--- app/src/main/java/com/truizlop/wearnotificationsamples/HomeActivity.java
+++ app/src/main/java/com/truizlop/wearnotificationsamples/HomeActivity.java
@@ -1,7 +1,16 @@
 package com.truizlop.wearnotificationsamples;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +18,36 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.plain_notification_button)
+    public void onSendPlainNotificationClicked(){
+        sendPlainNotification();
+    }
+
+    private void sendPlainNotification(){
+        String sender = "Jesse Pinkman";
+        String message = "Yo! Mr. White!";
+        @DrawableRes int image = R.drawable.pinkman;
+
+        Intent viewIntent = ViewMessageActivity.getLaunchIntent(this, sender, message, image);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_sms)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), image))
+                        .setContentTitle(sender)
+                        .setContentText(message)
+                        .setAutoCancel(true)
+                        .setContentIntent(viewPendingIntent);
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+        int notificationId = 100;
+        notificationManager.notify(notificationId, notificationBuilder.build());
     }
 }
