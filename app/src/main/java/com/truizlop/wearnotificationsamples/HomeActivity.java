81a584fc7b83754c6fbb09cd3625cc9e3bc4cf8f
Tomas Ruiz-Lopez <trl.ironside@gmail.com>
2016-02-13 10:41:07 +0100

    Add sending notification with action

--- app/src/main/java/com/truizlop/wearnotificationsamples/HomeActivity.java
+++ app/src/main/java/com/truizlop/wearnotificationsamples/HomeActivity.java
@@ -3,6 +3,7 @@ package com.truizlop.wearnotificationsamples;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.support.v4.app.NotificationCompat;
@@ -26,6 +27,11 @@ public class HomeActivity extends AppCompatActivity {
         sendPlainNotification();
     }
 
+    @OnClick(R.id.action_notification_button)
+    public void onSendActionNotificationClicked(){
+        sendNotificationWithAction();
+    }
+
     private void sendPlainNotification(){
         String sender = "Jesse Pinkman";
         String message = "Yo! Mr. White!";
@@ -33,7 +39,7 @@ public class HomeActivity extends AppCompatActivity {
 
         Intent viewIntent = ViewMessageActivity.getLaunchIntent(this, sender, message, image);
         PendingIntent viewPendingIntent =
-                PendingIntent.getActivity(this, 0, viewIntent, 0);
+                PendingIntent.getActivity(this, 0, viewIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this)
@@ -48,6 +54,39 @@ public class HomeActivity extends AppCompatActivity {
                 NotificationManagerCompat.from(this);
 
         int notificationId = 100;
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+
+    private void sendNotificationWithAction(){
+        String sender = "Jesse Pinkman";
+        String message = "I am waiting for you!";
+        @DrawableRes int image = R.drawable.pinkman;
+
+        Intent viewIntent = ViewMessageActivity.getLaunchIntent(this, sender, message, image);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(this, 1, viewIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        String albuquerque = "35.085334,-106.605553";
+        Uri geoUri = Uri.parse("geo:0,0?q=" + Uri.encode(albuquerque));
+        mapIntent.setData(geoUri);
+        PendingIntent mapPendingIntent =
+                PendingIntent.getActivity(this, 2, mapIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_sms)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), image))
+                        .setContentTitle(sender)
+                        .setContentText(message)
+                        .setAutoCancel(true)
+                        .setContentIntent(viewPendingIntent)
+                        .addAction(R.drawable.ic_map, getString(R.string.map), mapPendingIntent);
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+        int notificationId = 200;
         notificationManager.notify(notificationId, notificationBuilder.build());
     }
 }
